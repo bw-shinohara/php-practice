79368d2f93edad7477aecfd9556214f788940846
Your Name <you@example.com>
2025-06-14 07:50:06 +0900

    Sec6のQ1まで解いた

--- php-practice/php-practice_2.php
+++ php-practice/php-practice_2.php
@@ -1,6 +1,25 @@
 <?php
 // Q1 tic-tac問題
+echo '1から100までのカウントを開始します' . "\n";
 
+for ($i = 1 ; $i <= 100 ; $i++)
+{
+  if ($i % 4 === 0 && $i % 5 === 0)
+  {
+    echo 'tic-tac' . "\n";
+  }elseif
+  ($i % 4 === 0)
+  {
+    echo 'tic' . "\n";
+  }elseif
+  ($i % 5 === 0)
+  {
+    echo  'tac' . "\n";
+  }else
+  {
+    echo $i. "\n";
+  } 
+}
 
 // Q2 多次元連想配列
 
