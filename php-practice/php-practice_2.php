73510b6710a8d1cdac5607a3485b4154da19efed
Your Name <you@example.com>
2025-06-14 09:04:33 +0900

    Sec6のQ2の問題2まで解いた

--- php-practice/php-practice_2.php
+++ php-practice/php-practice_2.php
@@ -22,7 +22,34 @@ for ($i = 1 ; $i <= 100 ; $i++)
 }
 
 // Q2 多次元連想配列
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+];
 
+var_dump($personalInfos);
+
+// 問題1
+echo $personalInfos[1]['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。';
+
+// 問題2
+foreach ($personalInfos as $x => $y)
+{
+  echo $x + 1 . '番目の' .$personalInfos[$x]['name']. 'のメールアドレスは' .$personalInfos[$x]['mail']. 'で、電話番号は' .$personalInfos[$x]['tel']. 'です。';
+}
 
 // Q3 オブジェクト-1
 
